8be46b15e63766d51788d0bc8a89192a2a879cd0
Joel Costigliola <joel.costigliola@gmail.com>
2014-01-19 16:31:49 +0100

    refactor generate-html.py

--- generate-html.py
+++ generate-html.py
@@ -2,41 +2,27 @@ import os.path
 import string
 import glob
 
+
+def content_of(filename):
+    with open(filename) as file_to_read:
+        return file_to_read.read()
+
 print " "
 print "Generate assertj HTML files"
 
-# load content
-with open('assertj-top-menu.html') as menu_file:
-    menu_content = menu_file.read()
-with open('assertj-head.html') as head_file:
-    head_content = head_file.read()
-with open('assertj-footer.html') as footer_file:
-    footer_content = footer_file.read()
-with open('assertj-javascript.html') as javascript_file:
-    javascript_content = javascript_file.read()
-with open('assertj-core-side-menu.html') as assertj_core_side_menu_file:
-    assertj_core_side_menu_content = assertj_core_side_menu_file.read()
-with open('assertj-news-side-menu.html') as assertj_news_side_menu_file:
-    assertj_news_side_menu_content = assertj_news_side_menu_file.read()
-with open('assertj-guava-side-menu.html') as assertj_guava_side_menu_file:
-    assertj_guava_side_menu_content = assertj_guava_side_menu_file.read()
-with open('assertj-neo4j-side-menu.html') as assertj_neo4j_side_menu_file:
-    assertj_neo4j_side_menu_content = assertj_neo4j_side_menu_file.read()
-with open('assertj-joda-time-side-menu.html') as assertj_jodatime_side_menu_file:
-    assertj_jodatime_side_menu_content = assertj_jodatime_side_menu_file.read()
-with open('assertj-assertions-generator-side-menu.html') as assertj_assertions_generator_side_menu_file:
-    assertj_assertions_generator_side_menu_content = assertj_assertions_generator_side_menu_file.read()
 # stores it into a map for later substitution
-template_data_map = {'menu': menu_content,
-                     'head': head_content,
-                     'footer': footer_content,
-                     'javascript': javascript_content,
-                     'assertj_core_side_menu': assertj_core_side_menu_content,
-                     'assertj_news_side_menu': assertj_news_side_menu_content,
-                     'assertj_guava_side_menu': assertj_guava_side_menu_content,
-                     'assertj_neo4j_side_menu': assertj_neo4j_side_menu_content,
-                     'assertj_jodatime_side_menu': assertj_jodatime_side_menu_content,
-                     'assertj_assertions_generator_side_menu': assertj_assertions_generator_side_menu_content}
+template_data_map = {
+    'menu': content_of('assertj-top-menu.html'),
+    'head': content_of('assertj-head.html'),
+    'footer': content_of('assertj-footer.html'),
+    'javascript': content_of('assertj-javascript.html'),
+    'assertj_core_side_menu': content_of('assertj-core-side-menu.html'),
+    'assertj_news_side_menu': content_of('assertj-news-side-menu.html'),
+    'assertj_guava_side_menu': content_of('assertj-guava-side-menu.html'),
+    'assertj_neo4j_side_menu': content_of('assertj-neo4j-side-menu.html'),
+    'assertj_jodatime_side_menu': content_of('assertj-joda-time-side-menu.html'),
+    'assertj_assertions_generator_side_menu': content_of('assertj-assertions-generator-side-menu.html')
+}
 
 initial_dir = os.getcwd()
 templates_dir = "templates"
