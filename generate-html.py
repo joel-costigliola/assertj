d389cf7fbc187d8f3e02c16ec0307d8d51a126ee
croesch <christianroesch@gmx.net>
2014-03-02 20:56:22 +0100

    Add getting started.

--- generate-html.py
+++ generate-html.py
@@ -18,6 +18,7 @@ template_data_map = {
     'javascript': content_of('assertj-javascript.html'),
     'assertj_core_side_menu': content_of('assertj-core-side-menu.html'),
     'assertj_swing_side_menu': content_of('assertj-swing-side-menu.html'),
+    'assertj_swing_get_it': content_of('assertj-swing-get-it.html'),
     'assertj_news_side_menu': content_of('assertj-news-side-menu.html'),
     'assertj_guava_side_menu': content_of('assertj-guava-side-menu.html'),
     'assertj_neo4j_side_menu': content_of('assertj-neo4j-side-menu.html'),
